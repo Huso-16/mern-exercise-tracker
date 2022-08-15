e828a84eafbaec6e1dd63bc3b949a0d60a776b75
Abhishek Sahu <asahu.m1608@gmail.com>
2022-08-16 01:24:05 +0530

    Fix lint

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Main App Component is described here.
+ */
 class App extends Component {
+  /**
+   * Starts rendering the app
+   * @return {ReactNode} main app component
+   */
   render() {
     return (
       <div className="App">
