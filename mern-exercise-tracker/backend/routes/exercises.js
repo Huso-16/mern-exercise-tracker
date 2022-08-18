27cdf5eea8ff14d3eb1b3d9cde3ff24557f06f48
Abhishek Sahu <asahu.m1608@gmail.com>
2022-08-18 18:49:23 +0530

    Add more endpoints for exercises route
    - Get exercise by id
    - Update exercise by id
    - Delete exercise by id

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -10,6 +10,34 @@ ExercisesRouter.route('/').get((req, res) => {
       .catch((err) => res.status(400).json('Error: ' + err));
 });
 
+// Returns exercises of a user
+ExercisesRouter.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+      .then((exercise) => res.json(exercise))
+      .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+// Delete exercise
+ExercisesRouter.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise deleted.'))
+      .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+// Update exercise
+ExercisesRouter.route('/update/:id').post((req, res) => {
+  Exercise.findByIdAndUpdate(
+      req.params.id,
+      {
+        username: req.body.username,
+        description: req.body.description,
+        duration: Number(req.body.duration),
+        date: Date.parse(req.body.date),
+      })
+      .then(() => res.json('Exercise updated!'))
+      .catch((err) => res.status(400).json('Error: ' + err));
+});
+
 // Add new exercise
 ExercisesRouter.route('/add').post((req, res) => {
   const username = req.body.username;
