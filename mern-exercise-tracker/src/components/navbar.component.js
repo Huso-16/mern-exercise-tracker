ef167283bb8d94dd4429e4cde87d35c74be15116
Abhishek Sahu <39129685+ASahu16@users.noreply.github.com>
2022-08-25 19:16:40 +0530

    Update `CreateUser` component (#7)
    * Update `CreateUser` component
    
    - Add methods to update the component local state
    - Add a form to create a new user
    
    * Update `Navbar`
    
    Remove unnecessary class `collapse`

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -7,13 +7,13 @@ import {Link} from 'react-router-dom';
 export default class Navbar extends React.Component {
   /**
     * Component responsible to display the top navigation bar.
-    * @return {React.ReactNode} component to show the nabvar
+    * @return {React.ReactNode} component to show the navbar
     */
   render() {
     return (
       <nav className='navbar navbar-dark bg-dark navbar-expand-lg' >
         <Link to='/' className='navbar-brand'>Exercise Tracker</Link>
-        <div className='collapse navbar-collapse'>
+        <div className='navbar-collapse'>
           <ul className='navbar-nav mr-auto'>
             <li className='navbar-item'>
               <Link to="/" className='nav-link'>Exercises</Link>
